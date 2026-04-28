42c01f54997e703206f4dad9b1159edb0cce2e42
layssamello22-design <layssamello6@gmail.com>
2026-04-28 12:56:56 -0400

    Delete Lista 1 directory

--- Lista 1/Repeticao/ex009/script.js
+++
@@ -1,5 +0,0 @@
-/*9. Maior e Menor Idade
-Desafio: Faça um programa que leia a idade de 5 pessoas. Ao final do laço, o programa
-deve mostrar qual foi a maior e qual foi a menor idade digitada.
-• Dica: Inicialize a variável maiorIdade com 0 e menorIdade com um valor bem
-alto (como 999), ou defina ambas com o valor da primeira idade digitada.*/
